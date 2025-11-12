bb7d90425de67b346143b662b9f6bf7fef772062
Reid Matthews <reid.matthews@gmail.com>
2025-11-12 11:23:16 -0500

    Add waitForElement functions

--- tampermonkey-utils.js
+++ tampermonkey-utils.js
@@ -1,5 +1,42 @@
 console.log("Tampermonkey utilities loaded.");
 
 function bar() {
-    console.log("bar");
+  console.log("bar");
 }
+
+function waitForElement(selector, callback) {
+  const observer = new MutationObserver((mutations, observer) => {
+    const element = document.querySelector(selector);
+    if (element) {
+      observer.disconnect();
+      callback(element);
+    }
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
+}
+
+/* Usage
+waitForElement('#dynamicElement', (element) => {
+    console.log('Element exists:', element);
+    // Perform actions on the element
+});
+*/
+
+function waitForElementAsync(selector) {
+  return new Promise((resolve) => {
+    waitForElement(selector, (element) => {
+      resolve(element);
+    });
+  });
+}
+
+/* Usage
+waitForElement('#dynamicElement').then((element) => {
+    console.log('Element exists:', element);
+    // Perform actions on the element
+});
+*/
